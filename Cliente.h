268db177a41b25d56a57069b04d07d9f746c6100
Juliana Narcizo <42411447+julianarruda@users.noreply.github.com>
2019-06-14 10:18:48 -0300

    Update Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -3,14 +3,16 @@
 
 #include <string>
 #include <iostream>
+#define MAX 1000
 using namespace std;
 
 class Cliente{  //A classe cliente definirá os dados dos clientes cadastrados.
 private:
-    string _Pais;
-    string _DataCadastro;
+    string _Pais[MAX]; //Variável para o Pais de origem do cliente.
+    string _Data[MAX]; //Variável pata a Data de Cadastro do Cliente.
 public:
-    Cliente(string Pais, string DataCadastro);
+    Cliente();
+    Cliente(string Pais[MAX], string Data[MAX]);
     string get_Pais();
     string get_Data();
     virtual string Dados_Cliente();//Permite que os dados do cliente sejam inseridos para cadastro.

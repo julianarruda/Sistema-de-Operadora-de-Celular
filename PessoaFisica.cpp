a47b63c2b01210a9ce5de0f9d659b53fb33ffe1b
Juliana Narcizo <42411447+julianarruda@users.noreply.github.com>
2019-06-20 12:24:06 -0300

    Update PessoaFisica.cpp

--- PessoaFisica.cpp
+++ PessoaFisica.cpp
@@ -1,23 +1,33 @@
-#ifndef PESSOAFISICA_H_
-#define PESSOAFISICA_H_
+#include "PessoaFisica.h"
 
-#include<vector>
-#include "Cliente.h"
-
-class PessoaFisica: public Cliente{ //Faz o cadastro de pessoa fisica.
-protected:
-    string _nome;
-    string _CPF;
-    string _endereco;
-    vector <Celular>C;
-public:
-    PessoaFisica();
-    PessoaFisica(string nome, sting CPF, string endereco);
-    ~PessoaFisica();
-    string get_nome();
-    string get_CPF();
-    string get_endereco();
-    string Dados_Cliente(); //Insere os dados do cliente de pessoa fisica.
-};
-
-#endif // PESSOAFISICA_H_
+PessoaFisica::PessoaFisica(){
+}
+PessoaFisica::~PessoaFisica(){
+}
+PessoaFisica::PessoaFisica(string nome, string CPF, string endereco){ //Método para pessoa Física.
+    _nome = nome;
+    _CPF = CPF;
+    _endereco = endereco;
+}
+string PessoaFisica::get_nome(){
+    return this-> _nome;
+}
+string PessoaFisica::get_CPF(){
+    return this->_CPF;
+}
+string PessoaFisica::get_endereco(){
+    return this->_endereco
+}
+string PessoaFisica::DadosCliente(){
+    cout<< "Dados do Cliente"<<endl;
+    cout<<"Nome dO cliente:"<<endl;
+        cin>>_nome;
+        cout<<"CPF:"<<endl;
+        cin>>_CPF;
+        cout<<"Endereco do cliente:"<<endl;
+        cin>>_endereco;
+        cout<<endl;
+        cout<<"Nome do cliente: "<< _nome<<endl;
+        cout<<"CPF: "<< _CPF<<endl;
+        cout<<"Endereco: "<< _endereco<<endl;
+}

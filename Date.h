fb8b84031e144628617bebaabfb5d5f13be9ef8f
eduardaavila <50221826+eduardaavila@users.noreply.github.com>
2019-06-20 13:49:51 -0300

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,10 +1,11 @@
 #ifndef _DATE_H
 #define _DATE_H
-#include <string>
+
 #include <iostream>
 using namespace std;
 
 class Date{
+private:
     unsigned int dia;
     unsigned int mes;
     unsigned int ano;
@@ -12,9 +13,9 @@ class Date{
     unsigned int minuts;
 public:
     Date();
-    Date(unsigned int dia, unsigned int mes, unsigned int ano, unsigned int hora, unsigned int minuts);
+    Date(unsigned int _dia, unsigned int _mes, unsigned int _ano, unsigned int _hora, unsigned int _minuts);
     ~Date();
-    void data();
-    void hora();
-}
+    void diadomes(unsigned int _dia);
+    unsigned int get_diadomes();
+};
 #endif // _DATE_H
